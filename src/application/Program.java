5008d200f9936561f475fbbe72c81b5b62ab9cec
Jennifer Farias Rodrigues <jennifer.farias.rodrigues@gmail.com>
2020-08-26 16:44:52 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,18 @@
 package application;
 
+import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21,"Someone","some@gmail.com", new Date(),3000.00,obj);
+		System.out.println(seller);
 
 	}
 }
